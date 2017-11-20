71e906b66260f5ccc6e921d3a274e981a566aca0
BT_1N3_23 <BT_1N3_23@BT_1N3_23.bktech.local>
2017-11-20 13:14:34 -0500

    merge

--- Labs/src/lab3_2/ArrayMethods2.java
+++ Labs/src/lab3_2/ArrayMethods2.java
@@ -2,4 +2,11 @@ package lab3_2;
 
 public class ArrayMethods2 {
 
+	public static String[] merge(String[] list1, String[] list2) {
+		String[] result = new String[list1.length + list2.length];
+		while(true)
+		{
+			
+		}
+	}
 }
