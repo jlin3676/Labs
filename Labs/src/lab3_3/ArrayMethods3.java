a9dc540e4ce935e5d6098efefffa7b2d3f25a27a
BT_1N3_23 <BT_1N3_23@BT_1N3_23.bktech.local>
2017-11-28 12:03:38 -0500

    3.3 completed

--- Labs/src/lab3_3/ArrayMethods3.java
+++ Labs/src/lab3_3/ArrayMethods3.java
@@ -4,23 +4,40 @@ public class ArrayMethods3 {
 
 	public static void main(String[]args) {
 		String [] list1 = {"a","c","d","b","k","p","e","b"};
+		int [] list2 = {1,3,4,2,6,5,9,7,8};
+		double [] list3 = {3.14,6.89,13.37,4.20};
 		bubbleSort(list1);
-		printArray(list1);
+		insertionSort(list2);
+		selectionSort(list3);
+		printStringArray(list1);
+		printIntArray(list2);
+		printDoubleArray(list3);
 		
 	}
 	public static void insertionSort(int[]list1) {
-		int lowest = list1[0];
-		int lowestIdx = 0;
 		for(int i = 0; i < list1.length - 1; i++) {
-			for(int j = i+1; j < list1.length; j++) {
-				
+			int num = list1[i];
+			int j = i-1;
+			while(j >= 0 && list1[j] > num) {
+				list1[j+1] = list1[j];
+				j = j-1;
 			}
-			
+			list1[j+1] = num;
 		}
 	}
 	
 	public static void selectionSort(double[]list1) {
-		
+		int idx = 0;
+		for(int i = 0; i < list1.length-1; i++) {
+			for(int j = i+1; j < list1.length; j++) {
+				if(list1[j] < list1[i]) {
+					idx = j;
+				}
+			}
+			double temp = list1[idx];
+			list1[idx] = list1[i];
+			list1[i] = temp;
+		}
 	}
 	
 	public static void bubbleSort(String[]list1) {
@@ -35,7 +52,21 @@ public class ArrayMethods3 {
 		}
 	}
 	
-	public static void printArray(String[] arr) {
+	public static void printStringArray(String[] arr) {
+		for(int i = 0; i < arr.length; i++) {
+			System.out.print("[" + arr[i] + "] ");
+		}
+		System.out.println();
+	}
+	
+	public static void printIntArray(int[] arr) {
+		for(int i = 0; i < arr.length; i++) {
+			System.out.print("[" + arr[i] + "] ");
+		}
+		System.out.println();
+	}
+	
+	public static void printDoubleArray(double[] arr) {
 		for(int i = 0; i < arr.length; i++) {
 			System.out.print("[" + arr[i] + "] ");
 		}
