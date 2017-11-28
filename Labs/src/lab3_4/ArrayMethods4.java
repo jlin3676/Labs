607fa691255d349ca3c2177c0ed36f5b4011b9a3
BT_1N3_23 <BT_1N3_23@BT_1N3_23.bktech.local>
2017-11-28 13:14:50 -0500

    array4

---
+++ Labs/src/lab3_4/ArrayMethods4.java
@@ -0,0 +1,17 @@
+package lab3_4;
+
+public class ArrayMethods4 {
+
+	public static void partition(int[]list1, int front, int back) {
+		
+	}
+	
+	public static void quickSort(int[]list1, int front, int back) {
+		
+	}
+	
+	public static void customSort(/*depends on the sort*/){
+			
+	}
+	
+}
