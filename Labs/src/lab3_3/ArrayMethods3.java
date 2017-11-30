fbbee7646470b9843584e1a4266c4fa8ce3af1de
BT_1N3_23 <BT_1N3_23@BT_1N3_23.bktech.local>
2017-11-30 12:36:28 -0500

    selection done

--- Labs/src/lab3_3/ArrayMethods3.java
+++ Labs/src/lab3_3/ArrayMethods3.java
@@ -27,11 +27,14 @@ public class ArrayMethods3 {
 	}
 	
 	public static void selectionSort(double[]list1) {
+		double num;
 		int idx = 0;
 		for(int i = 0; i < list1.length-1; i++) {
+			num = list1[i];
 			for(int j = i+1; j < list1.length; j++) {
-				if(list1[j] < list1[i]) {
+				if(list1[j] < num) {
 					idx = j;
+					num = list1[j];
 				}
 			}
 			double temp = list1[idx];
