a555bc9720184c81fc058906eff33bc797d602d5
BT_1N3_24 <BT_1N3_24@BT_1N3_24.bktech.local>
2017-11-30 13:13:08 -0500

    partition

--- Labs/src/lab3_4/ArrayMethods4.java
+++ Labs/src/lab3_4/ArrayMethods4.java
@@ -2,22 +2,27 @@ package lab3_4;
 
 public class ArrayMethods4 {
 
-	public static void partition(int[]list1, int front, int back) {
-		int i = 1;
-		int j;
-		int pivot = list1[0];
-		int pos = 0;
-		int lastIdx;
-		while(list1[i] > pivot) {
-			i++;
-			if(list1[i] < pivot) {
-				int temp = list1[i];
-				list1[i] = list1[1];
-				list1[1] = temp;
-				lastIdx = i;
+	public static void main(String[]args) {
+		int[] list = {27,3,5,2,6,4,7};
+		System.out.println(partition(list,0,list.length));
+	}
+	public static int partition(int[]list1, int front, int back) {
+		int i = front;
+		int j = front + 1;
+		int pivot = list1[front];
+		while(j < back) {
+			if(list1[j] <= pivot){
+				i++;
+				int temp = list1[j];
+				list1[j] = list1[i];
+				list1[i] = temp;
 			}
+			j++;
 		}
-		
+		int temp = list1[i];
+		list1[i] = list1[front];
+		list1[front] = temp;
+		return i;
 	}
 	
 	public static void quickSort(int[]list1, int front, int back) {
@@ -28,4 +33,10 @@ public class ArrayMethods4 {
 			
 	}
 	
+	public static void printIntArray(int[] arr) {
+		for(int i = 0; i < arr.length; i++) {
+			System.out.print("[" + arr[i] + "] ");
+		}
+		System.out.println();
+	}
 }
