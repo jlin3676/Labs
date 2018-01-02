3e5a2b51114a39752e29b1e372eca0d55b194d79
BT_1N3_23 <BT_1N3_23@BT_1N3_23.bktech.local>
2018-01-02 13:11:23 -0500

    life

--- Labs/src/lab4_3/MinecraftPvP_named_by_brandon.java
+++ Labs/src/lab4_3/MinecraftPvP_named_by_brandon.java
@@ -2,4 +2,14 @@ package lab4_3;
 
 public class MinecraftPvP_named_by_brandon {
 
+	public static void main(String[]args) {
+		boolean gameOn = true;
+		while(gameOn) {
+			//game
+		}
+	}	
+	public int getAgeInSeconds() {
+        long nowMillis = System.currentTimeMillis();
+        return (int)((nowMillis - this.createdMillis) / 1000);
+	}
 }
