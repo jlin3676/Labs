c22b6fa85485c923dd86a28706f5dd67e9619b50
Jacky <jlin3676@bths.edu>
2018-01-21 18:27:09 -0500

    .

--- Labs/src/lab4_2/CSVUtilities.java
+++ Labs/src/lab4_2/CSVUtilities.java
@@ -1,7 +1,9 @@
 package lab4_2;
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -14,24 +16,19 @@ public class CSVUtilities {
 	private ArrayList<String> CSVData = new ArrayList<String>();
 	private int numColumns;
 	
-	public CSVUtilities(File csv) {
-		Path pathToFile = Paths.get(csv.getAbsolutePath());
+	public CSVUtilities(File csv) throws IOException {
+		Path pathToFile = Paths.get("2012_SAT_Results.csv");
 		try (BufferedReader br = Files.newBufferedReader(pathToFile,StandardCharsets.US_ASCII)){
 			String line = br.readLine();
 			while(line != null) {
 				this.CSVData.add(line);
 				line = br.readLine();
-				/*String[] data = line.split(",");
-				for(int i = 0; i < data.length; i++) {
-					this.CSVData.add(data[i]);
-				}
-				*/
 			}
 		}		
 				catch (IOException ioe) {
 				ioe.printStackTrace();
 			}
-		
+		this.numColumns = getColumnHeaders().size();
 	}
 	
 	public List<String> getColumnHeaders()
@@ -82,5 +79,6 @@ public class CSVUtilities {
 		}
 		return data;
 	}
-
+	
+	
 }
--- Labs/src/lab4_2/Runner.java
+++ Labs/src/lab4_2/Runner.java
@@ -8,18 +8,30 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+
+import lab4_2.CSVUtilities;
 public class Runner {
 
-	public static void main(String[]args) {
+	public static void main(String[]args) throws IOException {
+		
 		File csv = new File("2012_SAT_Results.csv");
 		CSVUtilities  theCSV = new CSVUtilities(csv);
 		
 		for(String a : theCSV.getColumnHeaders()) {
 			System.out.println(a);
 		}
-		/*for(String s : theCSV.getDataString(1)) {
+		for(String s : theCSV.getDataString(1)) {
 			System.out.println(s);
 		}
-		*/
+		
+		for (Integer b : theCSV.getDataInt(1)) {
+			System.out.println(b);
+		}
+		
+		for (Double c : theCSV.getDataDouble(1)) {
+			System.out.println(c);
+		}
 	}
+	
+	
 }
